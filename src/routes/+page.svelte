e804d62d90ae7d0e5e2123a3378c3e9f1797a258
yelunl <yeliz.unlu@hva.nl>
2023-11-28 19:50:43 +0100

    add slider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,10 @@
 </script>
 
 <main>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <section id="page1">
-
+    <h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </section>
 
 <section id="page2">
@@ -17,3 +16,27 @@
 </section>
 
 </main>
+
+<style>
+    main {
+        width: 100vw;
+        height: 100vh;
+        overflow: auto;
+        display: flex;
+        scroll-snap-type: x mandatory;
+        scroll-behavior: smooth;
+
+    }
+    section {
+        border: solid red;
+        width: 100vw;
+        height: 100vh;
+        flex-shrink: 0;
+        scroll-snap-align: start;
+        
+    }
+
+    #page1 {
+        width: 100vw;
+    }
+</style>
